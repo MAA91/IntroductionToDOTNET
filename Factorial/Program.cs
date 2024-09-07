480a38efcad01520d8e71f4ee3c255f9a0e320e1
MAA91 <Sanya13082007@gmail.com>
2024-09-07 14:23:53 +0300

    FactorialDone

---
+++ Factorial/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Numerics;
+
+namespace Factorial
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Factorial
+            #region
+            Console.Write("Введите число: ");
+            short n = Convert.ToInt16(Console.ReadLine());
+            if (n < 0)
+                Console.WriteLine("Factorial of a negative number is not defined");
+            else
+            {
+                double n_f = 1;
+                for (short i = 1; i <= n; i++, n_f *= i) ;
+                    Console.WriteLine($"{n}! = {n_f}");
+
+                BigInteger n_F = 1;
+                for (short i = 1; i <= n; i++, n_F *= i) ;
+                Console.WriteLine($"{n}! = {n_F}");
+            }
+            Console.WriteLine();
+            #endregion
+
+            //ромб
+            #region
+            Console.Write("Введите размер ромба: ");
+            int size = Convert.ToInt32(Console.ReadLine());
+            for (int i = -size; i <= size; i++)
+            {
+                if (i == 0)
+                    continue;
+                for (int j = -size; j <= size; j++)
+                {
+                    if ((-Math.Abs(j) + size == i) || (Math.Abs(j) - size == i))
+                    {
+                        if (i < 0)
+                        {
+                            if (j < 0)
+                                Console.Write('/');
+                            else if (j > 0)
+                                Console.Write(" \\");
+                            else
+                                Console.Write("/\\");
+                        }
+                        else if (i > 0)
+                        {
+                            if (j < 0)
+                                Console.Write('\\');
+                            else if (j > 0)
+                                Console.Write(" /");
+                            else
+                                Console.Write("\\/");
+                        }
+                    }
+                    else
+                        Console.Write(" ");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+            #endregion
+
+            //доска
+            #region
+            Console.Write("Введите размер доски: ");
+            int size_d = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < size_d * size_d;i++)
+            {
+                for(int j = 0; j < size_d * size_d; j++)
+                {
+                    if (Convert.ToBoolean((~((i / size_d % 2) | (j / size_d % 2)) % 2) | (~(~(i / size_d % 2) | ~(j / size_d % 2)) % 2))) 
+                        Console.Write("* ");
+                    else
+                        Console.Write("  ");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+            #endregion
+        }
+    }
+}
---
+++ Figure/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Figure")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("sborka")]
+[assembly: AssemblyProduct("Figure")]
+[assembly: AssemblyCopyright("Copyright © sborka 2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("fa2642b7-9009-412a-a04e-123f084fe3a7")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
